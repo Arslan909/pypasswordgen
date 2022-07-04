5092eb48742c8ef97722cac85e42c71165cf5ef3
shasherazi <hassanrandomz@gmail.com>
2022-07-05 00:15:00 +0500

    added -u to include uppercase letters

--- pypasswordgen/src/pypasswordgen.py
+++ pypasswordgen/src/pypasswordgen.py
@@ -11,25 +11,27 @@ parser.add_argument("-l",
                     type=int,
                     default=8,
                     help="Length of password")
+
 parser.add_argument("-u",
                     "--upper",
-                    type=str,
-                    default=string.ascii_uppercase,
-                    help="upper case character password")
+                    default=False,
+                    action="store_true",
+                    help="Include uppercase letters")
 
 args = parser.parse_args()
 
 
-def generate_password(length,upper):
+def generate_password(length, upper):
     """
     Generate a random password
     """
-    password = "".join(
-        random.choice(upper + string.digits)
-        for _ in range(length))
-    return password
+    if upper:
+        chars = string.ascii_letters + string.digits + string.punctuation
+    else:
+        chars = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(chars) for _ in range(length))
 
 
-if __name__ == "__main__":
-    print(generate_password(args.length,args.upper))
+if __name__ == '__main__':
+    print(generate_password(args.length, args.upper))
     exit(0)
