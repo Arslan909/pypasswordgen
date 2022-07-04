4344e7ff06f252388b907de0d92c32c70ca4d446
Arslan909 <Arslan909@gmail.com>
2022-07-04 14:52:32 +0500

    added upper case character feature

--- pypasswordgen/src/pypasswordgen.py
+++ pypasswordgen/src/pypasswordgen.py
@@ -11,20 +11,25 @@ parser.add_argument("-l",
                     type=int,
                     default=8,
                     help="Length of password")
+parser.add_argument("-u",
+                    "--upper",
+                    type=str,
+                    default=string.ascii_uppercase,
+                    help="upper case character password")
 
 args = parser.parse_args()
 
 
-def generate_password(length):
+def generate_password(length,upper):
     """
     Generate a random password
     """
     password = "".join(
-        random.choice(string.ascii_letters + string.digits)
+        random.choice(upper + string.digits)
         for _ in range(length))
     return password
 
 
 if __name__ == "__main__":
-    print(generate_password(args.length))
+    print(generate_password(args.length,args.upper))
     exit(0)
