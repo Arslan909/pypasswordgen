5eaa5b8573324750847bb3df9622d397d4014746
MTS999 <mtsprogrammer999@gmail.com>
2022-07-05 16:22:07 +0500

    default lowercase only

--- pypasswordgen/src/pypasswordgen.py
+++ pypasswordgen/src/pypasswordgen.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/u
+
 
 import random
 import argparse
@@ -9,7 +10,7 @@ parser = argparse.ArgumentParser(description="Generate a random password")
 parser.add_argument("-l",
                     "--length",
                     type=int,
-                    default=8,
+                    default=20,
                     help="Length of password")
 
 parser.add_argument("-u",
@@ -17,21 +18,52 @@ parser.add_argument("-u",
                     default=False,
                     action="store_true",
                     help="Include uppercase letters")
+parser.add_argument("-d",
+                    "--digit",
+                    default=False,
+                    action="store_true",
+                    help="Include digits")
+parser.add_argument("-p",
+                    "--punctuation",
+                    default=False,
+                    action="store_true",
+                    help="Include pupnctuation ")
 
 args = parser.parse_args()
 
 
-def generate_password(length, upper):
+def generate_password(length, upper,digit,punctuation):
     """
     Generate a random password
     """
-    if upper:
+    if upper and digit and punctuation:
+        chars = string.ascii_letters +string.digits+string.punctuation
+        
+    elif upper and digit:
+        chars = string.ascii_letters +string.digits
+        
+    elif upper and punctuation:
+        chars = string.ascii_letters+  string.punctuation  
+           
+    elif digit and punctuation:
+        chars = string.ascii_lowercase+string.digits +  string.punctuation 
+ 
+    elif upper:
         chars = string.ascii_letters
+        
+    elif digit:
+        chars = string.ascii_lowercase+string.digits
+        
+    elif punctuation:
+        chars = string.ascii_lowercase+string.punctuation
+        
     else:
         chars = string.ascii_lowercase
+        
     return ''.join(random.choice(chars) for _ in range(length))
 
 
 if __name__ == '__main__':
-    print(generate_password(args.length, args.upper))
+
+    print(generate_password(args.length, args.upper,args.digit,args.punctuation))
     exit(0)
